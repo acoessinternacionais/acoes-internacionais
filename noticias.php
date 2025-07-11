886424ac0ccf1e89633e83a3b8cf5c139216a619
Ações Internacionais <internacionaisacoes@gmail.com>
2025-07-11 16:42:36 -0300

    Update noticias.php

--- noticias.php
+++ noticias.php
@@ -1,1 +1,13 @@
-// Conteúdo exemplo para php/noticias.php
+<?php
+// noticias.php
+?>
+
+<section id="noticias" class="secao-noticias">
+  <h2 class="titulo-secao">📰 Notícias Recentes</h2>
+  <div id="noticias-container" class="noticias-container">
+    <p>Carregando notícias...</p>
+  </div>
+</section>
+
+<!-- Script de notícias -->
+<script src="js/noticias.js"></script>
