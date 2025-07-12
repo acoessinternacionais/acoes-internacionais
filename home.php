c9ebacaa93ff2350e5d83fcea03e8dd25b0195ff
Ações Internacionais <internacionaisacoes@gmail.com>
2025-07-12 11:54:17 -0300

    Update home.php

--- home.php
+++ home.php
@@ -8,14 +8,14 @@ $isLoggedIn = isset($_SESSION['usuario']);
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Ações Internacionais</title>
+  <title>Ações1 Internacionais</title>
   <link rel="stylesheet" href="style.css" />
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
 
 <header>
-  <h1>AÇÕES1 INTERNACIONAIS</h1>
+  <h1>AÇÕES INTERNACIONAIS</h1>
   <a href="<?= $isLoggedIn ? 'logout.php' : 'login.php' ?>">
     <?= $isLoggedIn ? 'Logout' : 'Login' ?>
   </a>
