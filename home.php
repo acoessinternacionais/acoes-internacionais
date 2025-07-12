35cad849655c38a38c31931acd588bea249b365c
Ações Internacionais <internacionaisacoes@gmail.com>
2025-07-12 12:00:47 -0300

    Update home.php

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ $isLoggedIn = isset($_SESSION['usuario']);
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Ações1 Internacionais</title>
+  <title>Ações3 Internacionais</title>
   <link rel="stylesheet" href="style.css" />
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
